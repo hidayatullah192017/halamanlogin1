f77b7df2e7417c082e46aa020d96cf11410825b4
hidayatullahdayat <hidayatullahdayat2002@gmail.com>
2023-11-02 17:23:48 +0700

    memperbaiki app isn't installed dan fungsi tombol hidden password di menu daftar

--- app/src/main/java/com/example/myapplication/DaftarActivity.kt
+++ app/src/main/java/com/example/myapplication/DaftarActivity.kt
@@ -3,9 +3,16 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.EditText
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import kotlinx.android.synthetic.main.activity_daftar.*
 
 class DaftarActivity : AppCompatActivity() {
+    private var passwordVisible = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daftar)
@@ -13,6 +20,7 @@ class DaftarActivity : AppCompatActivity() {
 
         btnBackDaftarListener()
         txtLoginListener()
+        passwordListener()
     }
 
     private fun btnBackDaftarListener() {
@@ -24,6 +32,35 @@ class DaftarActivity : AppCompatActivity() {
     private fun txtLoginListener() {
         txt_login.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
+    private fun passwordListener() {
+        val password = findViewById<EditText>(R.id.pass)
+
+        password.setOnTouchListener { view, motionEvent ->
+            val right = 2
+            if (motionEvent.action == MotionEvent.ACTION_UP) {
+                if (motionEvent.rawX >= password.right - password.compoundDrawables[right].bounds.width()) {
+                    val selection = password.selectionEnd
+                    if (passwordVisible) {
+                        // Set drawable image here
+                        password.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_visibility_off_24, 0)
+                        // For hide password
+                        password.transformationMethod = PasswordTransformationMethod.getInstance()
+                        passwordVisible = false
+                    } else {
+                        // Set drawable image here
+                        password.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_visibility_24, 0)
+                        // For show password
+                        password.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                        passwordVisible = true
+                    }
+                    password.setSelection(selection)
+                    return@setOnTouchListener true
+                }
+            }
+            false
         }
     }
 }
